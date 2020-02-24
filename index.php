5e4420fa87e9c475eb935bc5fddb85e756a035dc
promodsushil <61241840+promodsushil@users.noreply.github.com>
2020-02-24 21:40:50 +0600

    Created 3 package arrays and echoed 1 package with foreach loop.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+$pkg1 = [
+    'Lotion' => 300,
+    'Facewash' => 150,
+    'Soap' => 50,
+];
+
+$pkg2 = [
+    'Cream' => 100,
+    'Paste' => 100,
+    'Powder' => 50,
+];
+
+$pkg3 = [
+    'Brush' => 30,
+    'Comb' => 20,
+    'Oil' => 70,
+];
+
+//foreach ($pkg1 as $product => $price){
+//    echo "$product = $price<br>";
+//};
+
+?>
